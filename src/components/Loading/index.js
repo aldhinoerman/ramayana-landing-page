21c026806445e6aa40171fb8ae8315e8e45de3d5
aldhinoerman <aldynoerman@gmail.com>
2021-06-14 12:45:57 +0700

    clean code

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,9 +4,9 @@ import './Loading.scss';
 export const Loading = () => {
     return (
         <div className="container">
-                <div className="loading">
-                    <div></div>
-                    <div></div>
+            <div className="loading">
+                <div></div>
+                <div></div>
             </div>
         </div>
     )
--- src/components/Sidenav/ImageSlider.js
+++ src/components/Sidenav/ImageSlider.js
@@ -9,24 +9,24 @@ function ImageSlider() {
 
     return (
         <>
-        <div className="slider" onClick={() => setStyle('slides fade')}>
+            <div className="slider" onClick={() => setStyle('slides fade')}>
 
-            <a href="#slide-1">1</a>
-            <a href="#slide-2">2</a>
-            <a href="#slide-3">3</a>
+                <a href="#slide-1">1</a>
+                <a href="#slide-2">2</a>
+                <a href="#slide-3">3</a>
 
-            <div className={style}>
-                <div id="slide-1">
-                    <img src={CarouselImg} alt="carousel-img" />
-                </div>
-                <div id="slide-2">
-                    <img src={TopImg} alt="carousel-img" />
-                </div>
-                <div id="slide-3">
-                    <img src={CarouselImg} alt="carousel-img" />
+                <div className={style}>
+                    <div id="slide-1">
+                        <img src={CarouselImg} alt="carousel-img" />
+                    </div>
+                    <div id="slide-2">
+                        <img src={TopImg} alt="carousel-img" />
+                    </div>
+                    <div id="slide-3">
+                        <img src={CarouselImg} alt="carousel-img" />
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
